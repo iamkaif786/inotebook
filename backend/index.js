99fe8712334149dd92292e41d30f4daec07cf79b
Mohammadkaif Shaikh <66009452+iamkaif786@users.noreply.github.com>
2023-04-03 08:47:30 +0530

    New Changes 3 Apr 2023

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
 const connectToMongo = require('./db');
-const express = require('express')
+const express = require('express');
 connectToMongo();
-const app = express()
-const port = 5000
+const app = express();
+const port = 5000;
 
 app.use(express.json())
 
@@ -11,5 +11,5 @@ app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 })
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,7 +8,7 @@ router.post('/createuser', [
     body('name', 'Enter a valid name').isLength({ min: 3 }),
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password must be at least 5 Characters').isLength({ min: 5 })
-    
+
 ], async (req, res) => {
     //If there are errors, return Bad request and the errors
     const errors = validationResult(req);
@@ -18,7 +18,6 @@ router.post('/createuser', [
 
     //Check whether the user with this email exists already
     try {
-
         let user = await User.findOne({ email: req.body.email });
         if (user) {
             return res.status(400).json({ error: 'Sorry a user with this email is already exists' });
@@ -26,9 +25,8 @@ router.post('/createuser', [
         user = await User.create({
             name: req.body.name,
             password: req.body.password,
-            email: req.body.email,
+            email: req.body.email
         })
-
         res.json(user)
     } catch (error) {
         console.error(error.message);
