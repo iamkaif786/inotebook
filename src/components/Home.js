3b7df624c55cdfc7c20f93ae44911a536ac15e03
Mohammadkaif Shaikh <66009452+iamkaif786@users.noreply.github.com>
2023-04-10 09:53:11 +0530

    Input tag width 50%

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,12 +9,12 @@ const Home = () => {
         <form>
           <div className="mb-3">
             <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+            <input type="email" className="form-control" style={{width: "50%"}} id="exampleInputEmail1" aria-describedby="emailHelp" />
             <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
           </div>
           <div className="mb-3">
             <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-            <input type="password" className="form-control" id="exampleInputPassword1" />
+            <input type="password" className="form-control" style={{width: "50%"}} id="exampleInputPassword1" />
           </div>
           <div className="mb-3 form-check">
             <input type="checkbox" className="form-check-input" id="exampleCheck1" />
