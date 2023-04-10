fa82286db14cc8657096d572b1d350f5debb7671
Mohammadkaif Shaikh <66009452+iamkaif786@users.noreply.github.com>
2023-04-10 10:24:05 +0530

    Added hard coded notes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,8 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
+import noteContext from '../context/notes/noteContext';
 const Home = () => {
-
+  const context = useContext(noteContext);
+  const {notes, setNotes} = context;
   return (
     <div>
       <div className="container my-3">
@@ -9,12 +10,12 @@ const Home = () => {
         <form>
           <div className="mb-3">
             <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-            <input type="email" className="form-control" style={{width: "50%"}} id="exampleInputEmail1" aria-describedby="emailHelp" />
+            <input type="email" className="form-control" style={{ width: "50%" }} id="exampleInputEmail1" aria-describedby="emailHelp" />
             <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
           </div>
           <div className="mb-3">
             <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-            <input type="password" className="form-control" style={{width: "50%"}} id="exampleInputPassword1" />
+            <input type="password" className="form-control" style={{ width: "50%" }} id="exampleInputPassword1" />
           </div>
           <div className="mb-3 form-check">
             <input type="checkbox" className="form-check-input" id="exampleCheck1" />
@@ -25,6 +26,9 @@ const Home = () => {
       </div>
       <div className="container my-3">
         <h2> Your Notes </h2>
+        {notes.map((note)=>{
+          return note.title;
+        })}
       </div>
     </div>
   )
