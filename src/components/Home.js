764f735eca1daa6ca42e8bcc08429a7249a6f444
Mohammadkaif Shaikh <66009452+iamkaif786@users.noreply.github.com>
2023-04-11 08:20:01 +0530

    Noteitem Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,35 +1,30 @@
-import React, { useContext } from 'react'
-import noteContext from '../context/notes/noteContext';
+import React from 'react'
+
+import Notes from './Notes';
 const Home = () => {
-  const context = useContext(noteContext);
-  const {notes, setNotes} = context;
+  
   return (
     <div>
-      <div className="container my-3">
+      <div className="container my-3 bg-dark ms-5" style={{ height: "330px", width: "360px" }}>
         <h2> Add a note </h2>
         <form>
           <div className="mb-3">
-            <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-            <input type="email" className="form-control" style={{ width: "50%" }} id="exampleInputEmail1" aria-describedby="emailHelp" />
+            <label htmlFor="exampleInputEmail1" className="form-label text-light">Email address</label>
+            <input type="email" className="form-control" style={{ width: "70%" }} id="exampleInputEmail1" aria-describedby="emailHelp" />
             <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
           </div>
           <div className="mb-3">
-            <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-            <input type="password" className="form-control" style={{ width: "50%" }} id="exampleInputPassword1" />
+            <label htmlFor="exampleInputPassword1" className="form-label text-light">Password</label>
+            <input type="password" className="form-control" style={{ width: "70%" }} id="exampleInputPassword1" />
           </div>
           <div className="mb-3 form-check">
             <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-            <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+            <label className="form-check-label text-light" htmlFor="exampleCheck1">Check me out</label>
           </div>
-          <button type="submit" className="btn btn-primary">Login</button>
+          <button type="submit" className="btn btn-outline-primary ms-5">Login</button>
         </form>
       </div>
-      <div className="container my-3">
-        <h2> Your Notes </h2>
-        {notes.map((note)=>{
-          return note.title;
-        })}
-      </div>
+      <Notes />
     </div>
   )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,28 +1,73 @@
 import NoteContext from "./noteContext";
-import {useState} from "react";
+import { useState } from "react";
 
 const NoteState = (props) => {
     const notesInitial = [
         {
-          "_id": "642e3e74ddab369db1a72be5",
-          "user": "642ba76d93184f2df4cd8fe2",
-          "title": "New Note",
-          "description": "Please access the playlist",
-          "tag": "YouTube",
-          "date": "2023-04-06T03:37:24.256Z",
-          "__v": 0
+            "_id": "642e3e74ddab369db1a72be5",
+            "user": "642ba76d93184f2df4cd8fe2",
+            "title": "New Note",
+            "description": "Please access the playlist",
+            "tag": "YouTube",
+            "date": "2023-04-06T03:37:24.256Z",
+            "__v": 0
         },
         {
-          "_id": "643391f34014d505a1b05964",
-          "user": "642ba76d93184f2df4cd8fe2",
-          "title": "New Note",
-          "description": "Please access the playlist 2",
-          "tag": "YouTube",
-          "date": "2023-04-10T04:34:59.382Z",
-          "__v": 0
+            "_id": "643391f34014d505a1b05964",
+            "user": "642ba76d93184f2df4cd8fe2",
+            "title": "New Note",
+            "description": "Please access the playlist 2",
+            "tag": "YouTube",
+            "date": "2023-04-10T04:34:59.382Z",
+            "__v": 0
+        },
+        {
+            "_id": "642e3e74ddab369db1a72be5",
+            "user": "642ba76d93184f2df4cd8fe2",
+            "title": "New Note",
+            "description": "Please access the playlist",
+            "tag": "YouTube",
+            "date": "2023-04-06T03:37:24.256Z",
+            "__v": 0
+        },
+        {
+            "_id": "642e3e74ddab369db1a72be5",
+            "user": "642ba76d93184f2df4cd8fe2",
+            "title": "New Note",
+            "description": "Please access the playlist",
+            "tag": "YouTube",
+            "date": "2023-04-06T03:37:24.256Z",
+            "__v": 0
+        },
+        {
+            "_id": "642e3e74ddab369db1a72be5",
+            "user": "642ba76d93184f2df4cd8fe2",
+            "title": "New Note",
+            "description": "Please access the playlist",
+            "tag": "YouTube",
+            "date": "2023-04-06T03:37:24.256Z",
+            "__v": 0
+        },
+        {
+            "_id": "642e3e74ddab369db1a72be5",
+            "user": "642ba76d93184f2df4cd8fe2",
+            "title": "New Note",
+            "description": "Please access the playlist",
+            "tag": "YouTube",
+            "date": "2023-04-06T03:37:24.256Z",
+            "__v": 0
+        },
+        {
+            "_id": "642e3e74ddab369db1a72be5",
+            "user": "642ba76d93184f2df4cd8fe2",
+            "title": "New Note",
+            "description": "Please access the playlist",
+            "tag": "YouTube",
+            "date": "2023-04-06T03:37:24.256Z",
+            "__v": 0
         }
-      ]
-      const [notes, setnotes] = useState(notesInitial)
+    ]
+    const [notes, setnotes] = useState(notesInitial)
 
     return (
         <NoteContext.Provider value={{ notes, setnotes }}>
